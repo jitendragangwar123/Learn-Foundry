98e5c627df7d73dfe77cb6d4735fc31af6c73779
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-04-30 23:01:45 +0530

    feat:script for deplying the contrtact

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external returns(Counter){
+        vm.startBroadcast();
+        Counter counter = new Counter();
+        vm.stopBroadcast();
+        return counter;
     }
 }
