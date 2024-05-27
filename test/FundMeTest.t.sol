4ae61369891a208b5155f51a0ac1e563a43add70
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-05-28 00:15:03 +0530

    feat:testing for withdraw funds by multiple funders

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -94,4 +94,28 @@ contract FundMeTest is Test {
         assertEq(endingFundMeBalance, 0);
         assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
     }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // arrange
+        // use uint160 to generate the address using number
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for(uint160 i =startingFunderIndex; i<= numberOfFunders;i++){
+            // hoax() is the combination of vm.prank() and vm.deal()
+            hoax(address(i),SEND_VALUE);
+            // console.log(address(i));
+            fundMe.fund{value:SEND_VALUE}();
+        }
+
+        //arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assertEq(address(fundMe).balance,0);
+        assertEq(startingOwnerBalance+startingFundMeBalance,fundMe.getOwner().balance);
+    }
 }
